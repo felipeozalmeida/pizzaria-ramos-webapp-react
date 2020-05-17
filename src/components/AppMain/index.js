718f0fc90b26b99d1aca4d43b658694fecfa1a42
Felipe Almeida <felipeortalmeida@gmail.com>
2020-05-17 13:50:05 -0300

    Bootstrapped main section

---
+++ src/components/AppMain/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+
+function AppMain() {
+  return (
+    <main>
+      <Container>
+        <h2 className="sr-only">Pizzas</h2>
+      </Container>
+    </main>
+  )
+}
+
+export default AppMain
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AppHeader from '../../components/AppHeader';
+import AppMain from '../../components/AppMain';
 import './styles.scss';
 
 function App() {
   return (
     <React.Fragment>
       <AppHeader />
+      <AppMain />
     </React.Fragment>
   );
 }
