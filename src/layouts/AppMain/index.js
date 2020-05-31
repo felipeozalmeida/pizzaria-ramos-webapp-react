3179216705dba2aba91b3d4cd6b44b441e11f3f1
Felipe Almeida <felipeortalmeida@gmail.com>
2020-05-30 21:16:38 -0300

    Added modal display after click

--- src/layouts/AppMain/index.js
+++ src/layouts/AppMain/index.js
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 import PizzaToolbar from "../../components/PizzaToolbar";
 import PizzaList from "../../components/PizzaList";
 
 function AppMain() {
-  function handlePizzaView() {
-    return console.log("show modal fucker!");
-  }
+  const handlePizzaView = () => setShowPizza(true);
+  const handlePizzaClose = () => setShowPizza(false);
 
+  const [showPizza, setShowPizza] = useState(false);
   const [pizzas] = useState([
     {
       id: 1,
@@ -54,11 +55,28 @@ function AppMain() {
     },
   ]);
 
+  // See issue for why modal animation is currently disabled
+  // https://github.com/react-bootstrap/react-bootstrap/issues/5075
+
   return (
     <main>
       <Container>
         <PizzaToolbar />
         <PizzaList pizzas={pizzas} onPizzaView={handlePizzaView} />
+        <Modal show={showPizza} onHide={handlePizzaClose} animation={false}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handlePizzaClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handlePizzaClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </Container>
     </main>
   );
