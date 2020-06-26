93e5c1136d6580ee6398bd62e7356d4ac4ce7c29
Felipe Almeida <felipeortalmeida@gmail.com>
2020-06-25 23:44:51 -0300

    Renamed to match actual Bootstrap component

--- src/components/PizzaItem/index.jsx
+++ src/components/PizzaItem/index.jsx
@@ -7,7 +7,7 @@ const PizzaItem = (props) => {
   return (
     <Card className="text-light">
       <Card.Img src={props.pizza.imageFile} alt={props.pizza.imageAlt} />
-      <Card.ImgOverlay className="PizzaItemOverlay">
+      <Card.ImgOverlay className="PizzaItemImgOverlay">
         <Card.Title className="PizzaItemTitle">{props.pizza.name}</Card.Title>
         <Card.Subtitle>{props.pizza.price}</Card.Subtitle>
         <PizzaItemToolbar pizza={props.pizza} onPizzaView={props.onPizzaView} />
