ca738297bb14936d29e9fe74878ae22c161d7680
Felipe Almeida <felipeortalmeida@gmail.com>
2020-06-25 23:20:57 -0300

    Added overlay to improve acessibility

--- src/components/PizzaItem/index.jsx
+++ src/components/PizzaItem/index.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import PizzaItemToolbar from "../PizzaItemToolbar";
+import "./styles.scss";
 
 const PizzaItem = (props) => {
   return (
     <Card className="text-light">
       <Card.Img src={props.pizza.imageFile} alt={props.pizza.imageAlt} />
-      <Card.ImgOverlay>
+      <Card.ImgOverlay className="PizzaItemOverlay">
         <Card.Title>{props.pizza.name}</Card.Title>
         <Card.Subtitle>{props.pizza.price}</Card.Subtitle>
         <PizzaItemToolbar pizza={props.pizza} onPizzaView={props.onPizzaView} />
