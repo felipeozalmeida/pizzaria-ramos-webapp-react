aae2a8dcbc9fa6530bb4ac02738ff899d0fea0e9
Felipe Almeida <felipeortalmeida@gmail.com>
2020-06-25 23:31:16 -0300

    Improved spacing between items

--- src/components/PizzaList/index.jsx
+++ src/components/PizzaList/index.jsx
@@ -6,9 +6,9 @@ import PizzaItem from "../../components/PizzaItem";
 const PizzaList = (props) => {
   if (props.pizzas.length) {
     return (
-      <Row className="mb-n3">
+      <Row className="mx-n2 mb-n3">
         {props.pizzas.map((pizza) => (
-          <Col className="mb-3" xs={6} sm={4} md={3} lg={2} key={pizza.id}>
+          <Col className="px-2 mb-3" xs={6} sm={4} md={3} lg={2} key={pizza.id}>
             <PizzaItem pizza={pizza} onPizzaView={props.onPizzaView} />
           </Col>
         ))}
